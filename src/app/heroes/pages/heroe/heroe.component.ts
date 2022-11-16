cbaec9fc4feaa820ecae8315b8cf360f59de720e
Daniel <daniel.morocho@simetrik.com>
2022-11-16 09:28:35 -0500

    Lazy loading and dynamic modules practice

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => console.log(id));
+  }
+}
